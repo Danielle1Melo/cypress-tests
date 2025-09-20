1c6b5635681847ab2f064c432ab6be771f7f022d
Danielle Silva de Melo <danielle.melo@estudante.ifro.edu.br>
2025-09-19 20:31:11 -0400

    add authentication tests

--- cypress/e2e/jornadaInicioTransacao.cy.js
+++ cypress/e2e/jornadaInicioTransacao.cy.js
@@ -2,9 +2,42 @@
 /// <reference types="cypress" />
 
 describe('Home Page', () => {
+  let authToken;
+  let saldo;
+  let transacoes;
+  let ultimaTransacao;
+
   beforeEach(() => {
     cy.visit('/')
+    cy.login('neilton@alura.com', '123456')
+    cy.url().should('eq', 'http://localhost:3000/home')
+    let saldoInicial = null
   })  
+
+  it('Deve retornar token de autenticacao da api usando cy.request', () => {
+    cy.request({method:'POST', url: 'http://localhost:8000/public/login', body: {
+      email: "neilton@alura.com",
+      senha: "123456"
+    }}).then((response) => {
+    authToken = response.body.access_token;
+    expect(authToken).is.not.null
+  })
+  })
+
+    it('Deve retornar token de autenticacao da api usando cy.request', () => {
+    cy.request({
+      method:'GET', 
+      url: 'http://localhost:8000/saldo', 
+      headers: {
+          'Authorizaiton': `Bearer ${authToken}`
+      }
+    }).then((response) => {
+    expect(response.body).is.not.null
+    expect(response.status).to.eq(200)
+    saldo = response.body;
+    console.log(saldo.valor)
+  })
+  })
 
   it('deve permitir que o usuário acesse a aplicação, realize uma transação e faça logout', () => {
     cy.login('neilton@alura.com', '123456');
